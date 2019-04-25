51b26cab89635a38291293fb4877df7e3337acfe
Lieema <axel.vigny@epita.fr>
2019-04-25 20:19:31 +0200

    everything done except last

--- app/src/main/java/fr/epita/android/hellogames/SecondActivity.kt
+++ app/src/main/java/fr/epita/android/hellogames/SecondActivity.kt
@@ -1,12 +1,14 @@
 package fr.epita.android.hellogames
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -41,7 +43,21 @@ class SecondActivity : AppCompatActivity() {
             override fun onResponse(call: Call<gameDetails>, response: Response<gameDetails>) {
                 if (response.code() == 200){
                     val responsedata = response.body()
-                    
+                    if (responsedata != null){
+                        nameText.text = responsedata.name
+                        typeText.text = responsedata.type
+                        nbplayersText.text = responsedata.players.toString()
+                        yearText.text = responsedata.year.toString()
+                        Glide.with(applicationContext)
+                            .load(responsedata.picture)
+                            .into(imageView5)
+                        textView10.text = responsedata.description_en
+                        button.setOnClickListener {
+                            val i = Intent(Intent.ACTION_VIEW)
+                            i.data = Uri.parse(responsedata.url)
+                            startActivity(i)
+                        }
+                    }
                 }
             }
         }
